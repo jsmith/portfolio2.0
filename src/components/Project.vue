b372572f20a11e4b1ef1e44f6d0c3163aa32fe80
Jacob Smith <jacob.smith@unb.ca>
2020-03-17 13:18:36 -0300

    more css to fix mobile

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,9 +1,5 @@
 <template>
-  <div 
-    class="project p-1 relative"
-    :class="{ 'project-show': show }"
-    @click="show = !show"
-  >
+  <div class="project p-1 relative">
     <div 
       class="h-full w-full shadow-2xl"
       :class="{ 'bg-center bg-cover': !project.stretch }"
@@ -12,21 +8,21 @@
         'background-size': project.stretch ? '100% 100%' : '',
       }"
     ></div>
-    <div class="absolute invisible inset-0 project-information m-8 p-2 flex flex-col shadow-xl">
-      <div class="text-2xl tracking-widest">{{ project.name }}</div>
+    <div class="absolute invisible inset-0 project-information m-8 md:m-6 p-2 flex flex-col shadow-xl">
+      <div class="text-2xl md:text-lg tracking-widest">{{ project.name }}</div>
       <div class="mt-1 text-xs">
         <div 
           v-for="tag in project.tags"
           :key="tag"
-          class="project-tag select-none inline-block rounded-full mb-1 mr-1 px-3 py-1"
+          class="project-tag select-none inline-block rounded-full mb-1 mr-1 px-3 py-1 md:px-2 md:py-0"
         >
           {{ tag }}
         </div>
       </div>
-      <div class="mt-2">{{ project.description }}</div>
+      <div class="mt-2 md:text-sm">{{ project.description }}</div>
       <div class="flex-grow"></div>
-      <a class="project-link" :href="project.link" target="_blank">$ start $WEBSITE</a>
-      <a class="project-link" :href="project.repo" target="_blank">$ start $REPOSITORY</a>
+      <a class="project-link md:text-sm" :href="project.link" target="_blank">$ start $WEBSITE</a>
+      <a class="project-link md:text-sm" :href="project.repo" target="_blank">$ start $REPOSITORY</a>
     </div>
   </div>
 </template>
@@ -39,10 +35,6 @@ export default createComponent({
   name: 'Project',
   props: {
     project: { type: Object as () => Project, required: true },
-  },
-  setup() {
-    const show = ref(false);
-    return { show };
   },
 });
 </script>
@@ -61,7 +53,7 @@ export default createComponent({
   }
 }
 
-.project:hover > .project-information, .project-show > .project-information {
+.project:hover > .project-information, .project:active .project-information {
   visibility: visible;
   background-color: var(--background1);
 }
@@ -75,7 +67,7 @@ export default createComponent({
   // text-decoration: none;
   color: inherit;
 
-  &:hover {
+  &:hover, &:active {
     color: #b11212;
   }
 }
